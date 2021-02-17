8dee6015fc80d26a658a069a7f91602a20fc243a
Gad Jacobs <gadexofficial@gmail.com>
2021-02-17 02:09:52 +0100

    Configuration and landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [showQuiz, setShowQuiz] = useState(false);
+
+  const toggleShow = (show) => {
+    setShowQuiz(!show);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        {showQuiz ? (
+          <div>Quiz stuff</div>
+        ) : (
+          <>
+            <h1 className="App-logo text-8xl font-black">?</h1>
+            <p>Welcome to our not-so-fancy quiz-maker/taker.</p>
+            <button
+              onClick={() => toggleShow(showQuiz)}
+              className="App-link text-3xl p-4 underline focus:outline-none"
+            >
+              Create Quiz
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
